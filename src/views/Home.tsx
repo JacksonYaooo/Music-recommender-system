697d88ce1f64d8f895dd6529411361ecf3882e83
huangmengyao <2594838054@qq.com>
2024-04-18 22:28:17 +0800

    feat: 重新确定环境版本

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,8 @@
-import { PropType, defineComponent } from "vue";
+import { defineComponent } from "vue";
 import s from "./Home.module.scss";
-
+import { ref } from 'vue'
 export const Home = defineComponent({
- setup(props, context) {
-   return () => (
-      <div>Home</div>
-    );
-  }
-})
+  setup(props, context) {
+    return () => <div>Home</div>;
+  },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "true",
+  },
   plugins: [
     vue(),
     vueJsx({
       transformOn: false,
-      mergeProps: false
-    })
-  ]
-})
+      mergeProps: false,
+    }),
+  ],
+});
