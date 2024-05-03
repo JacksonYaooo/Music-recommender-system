ad1576f5c8f26260fe5d22eb9112e0c045168223
huangmengyao <2594838054@qq.com>
2024-05-03 23:31:34 +0800

    feat: 完成推荐榜单

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import './assets/css/reset.css'
-import './assets/css/vars.scss'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import "./assets/css/reset.css";
+import "./assets/css/vars.scss";
+import "./assets/css/icon.css";
 
-import router from './router'
-import { App } from './App'
+import router from "./router";
+import { App } from "./App";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.use(ElementPlus)
-app.mount('#app')
-
+app.use(router);
+app.use(ElementPlus);
+app.mount("#app");
